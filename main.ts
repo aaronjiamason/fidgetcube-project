d02579333dd88b7e4491ec9035dcf0afc8685e93
aaronjiamason <122560100+aaronjiamason@users.noreply.github.com>
2023-01-20 13:29:40 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,68 @@
-basic.forever(function () {
-    music.playTone(Note.E, 250)
-    music.playTone(Note.E,500)
-    music.playTone(Note.E, 500)
-    music.playTone(Note.C,250)
-    music.playTone(Note.E,500)
-    music.playTone(Note.G,1000)
-    music.playTone(Note.G3,1000)
-    music.playTone(Note.C,750)
-    music.playTone(Note.G3,750)
-    music.playTone(Note.E3,750)
-    music.playTone(Note.A3,500)
-    music.playTone(Note.B3,500)
-    music.playTone(Note.Bb3,250)
-    music.playTone(Note.A3,500)
-    music.playTone(Note.G3,250)
-    music.playTone(Note.E,500)
-    music.playTone(Note.G,250)
-    music.playTone(Note.A,500)
-    music.playTone(Note.F,250)
-    music.playTone(Note.G,500)
-    music.playTone(Note.E,500)
-    music.playTone(Note.C,250)
-    music.playTone(Note.D,250)
-    music.playTone(Note.B3,500)
+let hand = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showString("yes")
+})
+input.onGesture(Gesture.ScreenDown, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+    music.playTone(330, 250)
+    music.playTone(330, 500)
+    music.playTone(330, 500)
+    music.playTone(262, 250)
+    music.playTone(330, 500)
+    music.playTone(392, 1000)
+    music.playTone(196, 1000)
+    music.playTone(262, 750)
+    music.playTone(196, 750)
+    music.playTone(165, 750)
+    music.playTone(220, 500)
+    music.playTone(247, 500)
+    music.playTone(233, 250)
+    music.playTone(220, 500)
+    music.playTone(196, 250)
+    music.playTone(330, 500)
+    music.playTone(392, 250)
+    music.playTone(440, 500)
+    music.playTone(349, 250)
+    music.playTone(392, 500)
+    music.playTone(330, 500)
+    music.playTone(262, 250)
+    music.playTone(294, 250)
+    music.playTone(247, 500)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("no")
+})
+input.onGesture(Gesture.Shake, function () {
+    hand = randint(1, 3)
+    if (hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (hand == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
+input.onScreenDown(function () {
+music.playTone(Note.G, 500)
+music.playTone(Note.C, 500)
 })
